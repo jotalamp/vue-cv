7bacd368983ad5c8ed1a34e94f8be3ec35817105
Jotalamp <joonaslampinen1@gmail.com>
2025-05-26 21:48:50 +0300

    ./dist added

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+module.exports = {
+    publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+};
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
 })
